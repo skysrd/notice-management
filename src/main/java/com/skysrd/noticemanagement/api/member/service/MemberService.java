61de8ebb5f68da38440ad2151f39dbfa7ee4dc19
skysrd <skysrd@gmail.com>
2023-07-10 05:35:16 +0900

    Feat: MemberServiceTest Try

--- src/main/java/com/skysrd/noticemanagement/api/member/service/MemberService.java
+++ src/main/java/com/skysrd/noticemanagement/api/member/service/MemberService.java
@@ -6,15 +6,18 @@ import com.skysrd.noticemanagement.api.member.domain.response.MemberResponse;
 import com.skysrd.noticemanagement.api.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
     //사용자 관리 서비스
 
     private final MemberRepository memberRepository;
 
     //사용자 추가 기능
+    @Transactional
     public MemberResponse signup(MemberSignupRequest memberSignupRequest) {
         //사용자 인스턴스 생성
         Member member = MemberSignupRequest.toMember(memberSignupRequest);
