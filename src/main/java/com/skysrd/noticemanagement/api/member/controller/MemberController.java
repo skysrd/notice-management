e9ac1103dc31999d0e0784023c9891e8a5d18c42
skysrd <skysrd@gmail.com>
2023-07-12 17:51:20 +0900

    Feat: member test

--- src/main/java/com/skysrd/noticemanagement/api/member/controller/MemberController.java
+++ src/main/java/com/skysrd/noticemanagement/api/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.skysrd.noticemanagement.api.member.domain.request.MemberSignupRequest
 import com.skysrd.noticemanagement.api.member.service.MemberService;
 import com.skysrd.noticemanagement.common.result.ResponseHandler;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
