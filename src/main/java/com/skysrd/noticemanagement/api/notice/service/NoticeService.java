2458dfc01a3b7db6f3319e73b9de016208f8f194
skysrd <skysrd@gmail.com>
2023-07-14 05:11:15 +0900

    Fix: (#21) update Notice Return UUID 기존 updateNotice는 NoticeResponse를 Return하고 있었으나, 이를 Notice의 Id를 Return하도록 수정

--- src/main/java/com/skysrd/noticemanagement/api/notice/service/NoticeService.java
+++ src/main/java/com/skysrd/noticemanagement/api/notice/service/NoticeService.java
@@ -1,24 +1,19 @@
 package com.skysrd.noticemanagement.api.notice.service;
 
-import com.skysrd.noticemanagement.api.notice.domain.entity.Notice;
-import com.skysrd.noticemanagement.api.notice.domain.enums.NoticeErrorCode;
 import com.skysrd.noticemanagement.api.notice.domain.request.CreateNoticeRequest;
 import com.skysrd.noticemanagement.api.notice.domain.request.DeleteNoticeRequest;
 import com.skysrd.noticemanagement.api.notice.domain.request.UpdateNoticeRequest;
 import com.skysrd.noticemanagement.api.notice.domain.response.NoticeResponse;
-import com.skysrd.noticemanagement.common.exception.ApiException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 public interface NoticeService {
     public UUID createNotice(CreateNoticeRequest createNoticeRequest);
     public List<NoticeResponse> getNoticeList();
     public NoticeResponse getNoticeDetail(UUID noticeId);
-    public NoticeResponse updateNotice(UpdateNoticeRequest updateNoticeRequest);
+    public UUID updateNotice(UpdateNoticeRequest updateNoticeRequest);
     public void deleteNotice(DeleteNoticeRequest deleteNoticeRequest);
 }
